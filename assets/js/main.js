16a7b3c5895e8ac977333c0136cf0532fba26d2c
Maksim Tananin <tananin@yandex.ru>
2025-08-14 18:54:28 +0700

    Добавил Decap CMS и CI/CD для GitLab

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,40 @@
-import Alpine from 'alpinejs'
+// import Alpine from 'alpinejs'
+// import Swiper from 'swiper/bundle'
+// import 'swiper/css/bundle'
 
-window.Alpine = Alpine
-Alpine.start()
+// // Alpine JS
+// window.Alpine = Alpine
+// Alpine.start()
+
+// // Сделать Swiper глобальным
+// window.Swiper = Swiper
+
+// // Swiper
+// // document.addEventListener('DOMContentLoaded', () => {
+// //   new Swiper('.swiper', {
+// //     loop: true,
+// //     navigation: {
+// //       nextEl: '.swiper-button-next',
+// //       prevEl: '.swiper-button-prev',
+// //     },
+// //   })
+// // })
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   new Swiper('.team-swiper', {
+//     loop: false,
+//     slidesPerView: 'auto',
+//     spaceBetween: 0,
+//     navigation: {
+//       nextEl: '.s-button-next',
+//       prevEl: '.s-button-prev',
+//     },
+//     breakpoints: {
+//       1024: {
+//         slidesPerView: 4,
+//         spaceBetween: 0,
+//       },
+//     },
+//   })
+// })
